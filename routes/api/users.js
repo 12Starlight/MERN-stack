3cf406fa5ba0a3f004d2b4f7c03ddd07ef83cdcf
Dave <dave.gagnat@gmail.com>
2020-05-07 17:42:25 -0500

    Setup jsonwebtoken using jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs'); 
 const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
 
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route'}));
@@ -49,7 +50,23 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({ msg: 'Success!'})
+            // res.json({ msg: 'Success!'})
+            const payload = {
+              id: user.id,
+              handle: user.handle,
+              email: user.email 
+            }
+            jwt.sign( // Successfully created the jwt and sent it back to the user
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                })
+              }
+            )
           } else {
             return res.status(400).json({ password: 'Incorrect password' });
           }
