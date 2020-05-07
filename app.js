bd54fc43c17417c1626e0409d7aa7c65f8a6d667
Dave <dave.gagnat@gmail.com>
2020-05-07 13:57:01 -0500

    Updated app.js to create a user and save it to the database

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const db = require('./config/keys').mongoURI; // Gives us an object back, we wan
 const users = require('./routes/api/users'); // After we created our routes they need to be imported
 const notes = require('./routes/api/notes');
 const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 
 mongoose 
@@ -19,7 +20,13 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   // console.log(res); 
-  debugger; 
+  // debugger; 
+  const user = new User({
+    handle: 'starlight',
+    email: 'starlight@gmail.com',
+    password: 'starlight123'
+  })
+  user.save()
   res.send('Hello World! We are building a Google Keep clone')
 });
 
