330e45ffc66ec1d657e4dbd77765b7b3b3cbf1ec
Dave <dave.gagnat@gmail.com>
2020-05-06 22:08:29 -0500

    Updated app.js. to use routes and updated routes directory

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// Import Express Mongoose
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
 const db = require('./config/keys').mongoURI; 
+const users = require('./routes/api/users'); // After we created our routes they need to be imported
+const notes = require('./routes/api/notes');
+const bodyParser = require('body-parser');
 
 
 mongoose 
@@ -10,7 +14,17 @@ mongoose
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World! We are building a Google Keep clone'));
+
+
+// Express Router
+app.use('/api/users', users); // After we import the routes, we need to use them in app.js in order for them to work
+app.use('/api/notes', notes);
+
+
 
 const port = process.env.PORT || 5000;
 
