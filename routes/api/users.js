cfb4ff7a8c85d158aeeae2ff117b45697e1a2c0c
Dave <dave.gagnat@gmail.com>
2020-05-07 14:24:23 -0500

    Updated users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route'}));
+
+
+// Express Controller
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ msg: 'A user is already registered with that email' })
+      } else {
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+           password: req.body.password
+        })
+
+        newUser.save()
+          .then(user => res.send(user))
+          .catch(err => res.send(err));
+      }
+    })
+})
+
 
 
 module.exports = router; 
