55e93c439d39083c3a5c2546687e3d933e84a3d5
Dave <dave.gagnat@gmail.com>
2020-05-07 22:30:43 -0500

    Updated notes.js to include other routes for index, user, id

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -9,6 +9,30 @@ const Note = require('../../models/Note');
 
 // router.get('/test', (req, res) => res.json({ msg: 'This is the notes route'}));
 
+// Express Controller
+router.get('/', (req, res) => {
+  Note
+    .find() // Find by filtering by the parameter, no parameter, so get everything back
+    .sort({ date: -1})
+    .then(notes => res.json(notes))
+    .catch(err => res.status(400).json(err));
+});
+
+router.get('/user/:user_id', (req, res) => {
+  Note 
+    .find({ user: req.params.user_id })
+    .then(notes => res.json(notes))
+    .catch(error => res.status(404).json(error))
+});
+
+router.get('/:id', (req, res) => {
+  Note
+    .findById(req.params.id)
+    .then(note => res.json(note))
+    .catch(error => res.status(404).json(error)) 
+})
+
+
 router.post('/', 
   passport.authenticate('jwt', { session: false }),
   (req, res) => { 
