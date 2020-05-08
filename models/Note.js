61e87a2ec0c4d3b81fd3f9a3039307a460874831
Dave <dave.gagnat@gmail.com>
2020-05-07 20:47:36 -0500

    Created NoteSchema and exported it

---
+++ models/Note.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const NoteSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  title: {
+    type: String,
+    required: false
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+const Note = mongoose.model('notes', NoteSchema);
+module.exports = Note; 
