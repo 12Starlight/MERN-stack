91a0c5c49b82c028258774704fc9071037b6c18b
Dave <dave.gagnat@gmail.com>
2020-05-07 19:15:48 -0500

    Updated users controller, used Validator to validate user information with register

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ const User = require('../../models/User');
 const bcrypt = require('bcryptjs'); 
 const keys = require('../../config/keys');
 const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route'}));
@@ -12,8 +14,14 @@ router.get('/test', (req, res) => res.json({ msg: 'This is the users route'}));
 
 // Express Controller
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  
   User.findOne({ email: req.body.email })
-    .then(user => {
+    .then(user => { 
       if (user) {
         return res.status(400).json({ msg: 'A user is already registered with that email' })
       } else {
--- validation/login.js
+++ validation/login.js
@@ -24,6 +24,6 @@ module.exports = function(data) {
      
   return {
     errors,
-    isValid: Object.keys(errors).length === 0;
+    isValid: Object.keys(errors).length === 0
   }
 }
