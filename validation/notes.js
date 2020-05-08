cff9f60dcbee0209bd668545f7dfcd116a500eb8
Dave <dave.gagnat@gmail.com>
2020-05-07 20:56:09 -0500

    Setu up validations for notes.js

---
+++ validation/notes.js
@@ -0,0 +1,24 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+
+module.exports = function validateNoteInput(data) {
+  let errors = {};
+
+  data.title = validText(data.title) ? data.title : '';
+  data.text = validText(data.text) ? data.text : '';
+
+  if (!Validator.isLength(data.text), { min: 5, max: 255 }) {
+    errors.text = 'Note must be between 5 and 255 characters';
+  }
+
+  if (!Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
+}
