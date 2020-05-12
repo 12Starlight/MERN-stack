be292b87d1e9a5617cac5506ee70d74c8ad801e9
Dave <dave.gagnat@gmail.com>
2020-05-12 18:40:15 -0500

    Built note_compose.js

---
+++ frontend/src/components/notes/note_compose.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import NoteBox from './note_box';
+
+
+class NoteCompose extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      text: '',
+      newNote: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ newNote: { title: nextProps.newNote.title, text: nextProps.newNote.text } });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let note = {
+      title: this.state.title,
+      text: this.state.text
+    }
+
+    this.props.composeNote(note);
+    this.setState({text: '', title: ''})
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+
+  render() {
+    return(
+      <div>
+        <form>
+          <div>
+            <input 
+              type='textarea'
+              value={this.state.text}
+              onChange={this.update('text')}
+              placeholder='Write your note...'
+            />
+            <input type='submit' value='Submit' />
+          </div>
+        </form>
+        <br/>
+        <NoteBox text={this.state.newNote.text} title={this.state.newNote.title} />
+      </div>
+    )
+  }
+}
+
+
+export default NoteCompose; 
