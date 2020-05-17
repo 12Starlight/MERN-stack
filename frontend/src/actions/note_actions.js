013ed9577423229b6b8c108139b58f1c86797d9f
Dave <dave.gagnat@gmail.com>
2020-05-17 15:18:05 -0500

    Added constant, regular action, thunk action in actions for deletingNote

--- frontend/src/actions/note_actions.js
+++ frontend/src/actions/note_actions.js
@@ -1,9 +1,10 @@
-import { getNotes, getUserNotes, writeNote } from '../util/note_api_util';
+import { getNotes, getUserNotes, writeNote, deleteNote } from '../util/note_api_util';
 
 // Constants
 export const RECEIVE_NOTES = 'RECEIVE_NOTES';
 export const RECEIVE_USER_NOTES = 'RECEIVE_USER_NOTES';
 export const RECEIVE_NEW_NOTE = 'RECEIVE_NEW_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE'; 
 
 // Regular Action Creator
 export const receiveNotes = (notes) => ({
@@ -20,6 +21,11 @@ export const receiveNewNote = (note) => ({
   type: RECEIVE_NEW_NOTE,
   note
 });
+
+export const removedNote = (note) => ({
+  type: DELETE_NOTE,
+  noteId: note.id 
+})
 
 
 // Thunk Action Creator
@@ -38,6 +44,12 @@ export const fetchUserNotes = (id) => (dispatch) => (
 export const composeNote = (data) => (dispatch) => (
   writeNote(data)
     .then(note => dispatch(receiveNewNote(note)))
+    .catch(err => console.log(err))
+);
+
+export const deletedNote = (noteId) => (dispatch) => (
+  deleteNote(noteId)
+    .then(removed => dispatch(removedNote(removed)))
     .catch(err => console.log(err))
 );
 
